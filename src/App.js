ede17006c8b1f8a47217acd525c844df4cdff231
Anas <anas@Fatehs-MacBook-Pro.local>
2023-05-18 10:37:58 -0700

    reviews

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import api from './api/axiosConfig';
-import Layout from './components/layout';
 import {useState, useEffect} from 'react';
+import Layout from './components/layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
-
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+
 function App() {
 
   const [movies, setMovies] = useState();
-
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () =>{
     
     try
     {
 
-      const response = await api.get("/movies");
-      console.log(response.data);
+      const response = await api.get("/api/v1/movies");
+
       setMovies(response.data);
 
     } 
@@ -26,6 +28,27 @@ function App() {
     {
       console.log(err);
     }
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
@@ -37,8 +60,10 @@ function App() {
       <Header/>
       <Routes>
             <Route path="/" element={<Home movies={movies} />} ></Route>
-            <Route path="/Trailer/:ytTrailerId" element ={<Trailer/>}></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
       </Routes>
+
     </div>
   );
 }
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,21 @@
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import {Link, useNavigate} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
+    function reviews(movieId)
+    {
+        navigate(`/Reviews/${movieId}`);
+    }
+
   return (
     <div className ='movie-carousel-container'>
       <Carousel>
@@ -24,8 +32,7 @@ const Hero = ({movies}) => {
                                     <div className="movie-title">
                                         <h4>{movie.title}</h4>
                                     </div>
-                                    <div className='movie-buttons-container'>
-                                        <div className='play-button-icon-container'>
+                                    <div className="movie-buttons-container">
                                         <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                             <div className="play-button-icon-container">
                                                 <FontAwesomeIcon className="play-button-icon"
@@ -33,8 +40,10 @@ const Hero = ({movies}) => {
                                                 />
                                             </div>
                                         </Link>
-                                        </div>
 
+                                        <div className="movie-review-button-container">
+                                            <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
